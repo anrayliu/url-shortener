cc3de26ff07afe4208faa09c08680020d1d91628
Anray Liu <74158148+anrayliu@users.noreply.github.com>
2025-12-26 13:04:41 -0500

    feat: add iac (#13)
    * chore: update .gitignore
    
    * feat(iac): basic dev env template

---
+++ iac/terraform/dev.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc07"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = var.proxmox_endpoint
+  pm_tls_insecure = true
+}
+
+resource "proxmox_lxc" "dev" {
+    features {
+        nesting = true
+    }
+    hostname = "url-shortener-dev"
+    network {
+        name = "eth0"
+        bridge = "vmbr0"
+        ip = "dhcp"
+    }
+    rootfs {
+        storage = "local-lvm"
+        size    = "4G"
+    }
+    ostemplate = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+    cores = 1
+    memory = 3072
+    onboot = true
+    start = true
+    password = var.dev_root_password
+    target_node = "pve"
+    unprivileged = true
+}
