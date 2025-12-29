a3afdbd5a56d2bf046a45b46457914007bfea761
Anray Liu <anrayliu@gmail.com>
2025-12-29 17:57:56 -0500

    style(iac): fix terraform fmt and lint

--- iac/terraform/dev.tf
+++ iac/terraform/dev.tf
@@ -5,34 +5,35 @@ terraform {
       version = "3.0.2-rc07"
     }
   }
+  required_version = "~> 1.0"
 }
 
 provider "proxmox" {
-  pm_api_url = var.proxmox_endpoint
+  pm_api_url      = var.proxmox_endpoint
   pm_tls_insecure = true
 }
 
 resource "proxmox_lxc" "dev" {
-    features {
-        nesting = true
-    }
-    ssh_public_keys = var.ssh_keys
-    hostname = "url-shortener-dev"
-    network {
-        name = "eth0"
-        bridge = "vmbr0"
-        ip = var.dev_container_ip
-    }
-    rootfs {
-        storage = "local-lvm"
-        size    = "4G"
-    }
-    ostemplate = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
-    cores = 1
-    memory = 3072
-    onboot = true
-    start = true
-    password = var.dev_root_password
-    target_node = "pve"
-    unprivileged = true
+  features {
+    nesting = true
+  }
+  ssh_public_keys = var.ssh_keys
+  hostname        = "url-shortener-dev"
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = var.dev_container_ip
+  }
+  rootfs {
+    storage = "local-lvm"
+    size    = "4G"
+  }
+  ostemplate   = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+  cores        = 1
+  memory       = 3072
+  onboot       = true
+  start        = true
+  password     = var.dev_root_password
+  target_node  = "pve"
+  unprivileged = true
 }
--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "proxmox_endpoint" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "dev_root_password" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "dev_container_ip" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "ssh_keys" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
