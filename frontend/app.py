00a224ae0e0be8676e776a3740ec6c7ee63b33cd
Anray Liu <anrayliu@gmail.com>
2026-01-01 13:07:22 -0500

    fix: remove hostname resolving in flask

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,4 @@
 import os
-import socket
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
@@ -11,8 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    ip_address = socket.gethostbyname(os.environ["API_HOST"])
-    return render_template("index.html", ip_address=ip_address, port=os.environ["API_PORT"])
+    return render_template("index.html", ip_address=os.environ["API_HOST"], port=os.environ["API_PORT"])
 
 
 if __name__ == "__main__":
