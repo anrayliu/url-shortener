8172f25997d641be462197e8addcb094818d6f73
Anray Liu <anrayliu@gmail.com>
2026-01-01 12:15:51 -0500

    feat: resolve hostname in flask

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,5 @@
 import os
+import socket
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
@@ -10,7 +11,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", ip_address=os.environ["API_HOST"], port=os.environ["API_PORT"])
+    ip_address = socket.gethostbyname(os.environ["API_HOST"])
+    return render_template("index.html", ip_address=ip_address, port=os.environ["API_PORT"])
 
 
 if __name__ == "__main__":
