3728cd4f14fc38ddecee62f40afdf046c5f83996
Anray Liu <anrayliu@gmail.com>
2026-01-01 18:02:23 -0500

    debug: fix cicd unit test

--- backend/test_app.py
+++ backend/test_app.py
@@ -7,20 +7,20 @@ import unittest
 import psycopg2.pool
 from unittest.mock import patch
 
+import sys
+sys.modules.pop("app", None)
 
 class TestApi(unittest.TestCase):
-    @patch("psycopg2.pool.SimpleConnectionPool")
-    def test_shorten_no_body(self, mock_pool):
+    def test_shorten_no_body(self):
         # prevent connecting to database
-        mock_pool.return_value = None
+        with patch("psycopg2.pool.SimpleConnectionPool", return_value=None):
+            from app import app
 
-        from app import app
-
-        client = app.test_client()
-        
-        response = client.post("/api/v1/shorten", json={})
-        
-        self.assertEqual(response.status_code, 400)
+            client = app.test_client()
+            
+            response = client.post("/api/v1/shorten", json={})
+            
+            self.assertEqual(response.status_code, 400)
 
 
 if __name__ == "__main__":
