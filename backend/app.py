c840e997605b8fc8db1182cef2c057727a3cbd25
Anray Liu <anrayliu@gmail.com>
2026-01-01 16:16:46 -0500

    refactor(backend): improve error handling

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,18 @@
-from psycopg2.pool import SimpleConnectionPool
-from flask import Flask, jsonify, redirect, request
+import psycopg2
+import psycopg2.pool
+from flask import Flask, jsonify, redirect, request, abort
 from flask_cors import CORS
 from dotenv import load_dotenv
-import os
-import hashlib
+import os # environ()
+import hashlib # sha256()
+import logging # getLogger()
+import string # ascii_letters
+import random # choice()
 
 
 load_dotenv()
 
-pool = SimpleConnectionPool(
+pool = psycopg2.pool.SimpleConnectionPool(
     3, 20, # min and max connections
     database=os.environ["DB_NAME"],
     host=os.environ["DB_HOST"],
@@ -19,36 +23,61 @@ pool = SimpleConnectionPool(
 
 app = Flask(__name__)
 
+logger = logging.getLogger(__name__)
+
 CORS(app)
 
 
-def hash_url(url, cur):
+# grabs an available connection from the pool
+def get_connection():
+    try:
+        conn = pool.getconn()
+    except psycopg2.pool.PoolError as e:
+        logger.error(e)
+        abort(503, description="Connections at capacity")
+    
+    return conn
+
+def exec_query(conn, query, args, fetch=True):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(query, args)
+        except psycopg2.Error as e:
+            conn.rollback()
+            logger.error(e)
+            abort(500)
+        
+        if fetch:
+            return cur.fetchone()
+
+def hash_url(url, conn):
     exists = True
 
     while exists:
-        sha = hashlib.sha256(url.encode('utf-8')).hexdigest()[:7]
-        cur.execute("SELECT * FROM urls WHERE short_url = %s;", (sha,))
-        exists = cur.fetchone() is not None
-        url += "a"
+        # short url is the first 7 digits in a sha256 hash
+        sha = hashlib.sha256(url.encode("utf-8")).hexdigest()[:7]
+        exists = exec_query(conn, "SELECT * FROM urls WHERE short_url = %s;", (sha,)) is not None
+        
+        # adds a random letter to the long url if hash is a conflict
+        url += random.choice(string.ascii_letters)
 
     return sha
 
 @app.route("/api/v1/shorten", methods=["POST"]) 
 def handle_shorten():
     long_url = request.get_json().get("url")
+    if long_url is None:
+        abort(400, description="Missing 'long_url'")
 
-    conn = pool.getconn()
-    cur = conn.cursor()
+    conn = get_connection()
 
-    cur.execute("SELECT * FROM urls WHERE long_url = %s;", (long_url,))
-
-    # saved long_url, short_url pair in database
-    db_pair = cur.fetchone()
+    # find saved long_url, short_url pair in database
+    db_pair = exec_query(conn, "SELECT * FROM urls WHERE long_url = %s;", (long_url,))
 
     if db_pair is None:
-        short_url = hash_url(long_url, cur)
+        short_url = hash_url(long_url, conn)
 
-        cur.execute("INSERT INTO urls VALUES (%s, %s);", (long_url, short_url))
+        db_pair = exec_query(conn, "INSERT INTO urls VALUES (%s, %s);", (long_url, short_url), fetch=False)
         conn.commit()
 
     else:
@@ -67,21 +96,14 @@ def handle_shorten():
 
 @app.route("/api/v1/redirect/<url>")
 def handle_redirect(url):
-    conn = pool.getconn()
-    cur = conn.cursor()
+    conn = get_connection()
 
-    cur.execute("SELECT * FROM urls WHERE short_url = %s;", (url,))
-
-    db_pair = cur.fetchone()
+    db_pair =  exec_query(conn, "SELECT * FROM urls WHERE short_url = %s;", (url,))
 
     pool.putconn(conn)
 
     if db_pair is None:
-        response = {"error":{
-            "code": 400, 
-            "message": "Short URL does not exist."
-        }}
-        return jsonify(response)
+        abort(400, description="Short URL does not exist")
 
     redirect_url = db_pair[0]
     if not redirect_url.startswith("http://") and not redirect_url.startswith("https://"):
